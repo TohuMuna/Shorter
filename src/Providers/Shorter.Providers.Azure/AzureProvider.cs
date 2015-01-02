3231c89bc171d1ca71a9e8ac68a1fafd303283e8
Jon Levett <jon.levett@thap.co.uk>
2015-01-02 17:23:13 +0400

    Add azure provider with empty implementation

---
+++ src/Providers/Shorter.Providers.Azure/AzureProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace Shorter.Providers.Azure
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Shorter.Core.Dto;
+
+    public class AzureProvider : IProvider
+    {
+        public Task<ShortCode> GenerateShortCode(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShortCode> GetUrlFromShortCode(string shortCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
